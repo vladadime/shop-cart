82dbc6a70b31c14740c2490c1033c980dbe60fcf
vladadime <dime.97@hotmail.com>
2024-10-21 22:10:27 +0200

    Add initial implementation of Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,9 @@
-const Layout = () => {
-    return <div>Layout</div>
+const Layout = ({ children }) => {
+    return (
+        <div className='bg-white bg-[repeating-linear-gradient(to_right,_rgba(255,0,0,0.2),_rgba(255,0,0,0.2)_1px,_white_1px,_white_55px)] h-screen'>
+            {children}
+        </div>
+    )
 }
 
 export default Layout
