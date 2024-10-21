753f3f70de0d7bcc48aebc8be3dd80e5fa414a91
vladadime <dime.97@hotmail.com>
2024-10-21 22:11:28 +0200

    Add initial implementation of Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,12 @@
-const Button = () => {
-    return <div>Button</div>
+const Button = ({ content }) => {
+    return (
+        <button
+            className='bg-black text-white text-xs rounded-full px-10 py-2 w-full'
+            type='submit'
+        >
+            {content}
+        </button>
+    )
 }
 
 export default Button
