42132002383faffb72de02a2cbacff1f28ab24e7
vladadime <dime.97@hotmail.com>
2024-10-21 22:12:41 +0200

    Add Layout to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Cart, Login, Products } from './pages'
+import { Layout } from './components'
 
 function App() {
     return (
         <div className='App'>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Products />} />
-                    <Route path='/cart' element={<Cart />} />
-                    <Route path='/login' element={<Login />} />
-                </Routes>
-            </Router>
+            <Layout>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<Products />} />
+                        <Route path='/cart' element={<Cart />} />
+                        <Route path='/login' element={<Login />} />
+                    </Routes>
+                </Router>
+            </Layout>
         </div>
     )
 }
