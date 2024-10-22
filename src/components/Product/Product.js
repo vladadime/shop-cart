849d5f9e49b70629b2ed075e52c9e621244ee954
vladadime <dime.97@hotmail.com>
2024-10-22 22:09:32 +0200

    Add animation for update quantity and add to cart for one product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,14 @@
+import { useState } from 'react'
 const Product = ({ product }) => {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
     return (
         <div>
             <div className='relative'>
@@ -6,24 +16,35 @@ const Product = ({ product }) => {
                     className='w-3/4 object-contain mix-blend-multiply'
                     src={product.productImage}
                     alt={product.productLabel}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
                 />
-
-                <div className='absolute bottom-2 left-6 flex items-center rounded-md p-2'>
-                    <div className="rounded-full bg-white me-2 px-4">
-                        <div className="flex justify-around">
-                        <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
-                            -
-                        </button>
-                        <span className='mx-2 text-base my-auto'>1</span>
-                        <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
-                            +
+                {isHovered && (
+                    <div
+                        className={
+                            'absolute bottom-2 left-6 flex items-center rounded-md p-2'
+                        }
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <div className='rounded-full bg-white me-2 px-4'>
+                            <div className='flex justify-around'>
+                                <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
+                                    -
+                                </button>
+                                <span className='mx-2 text-base my-auto'>
+                                    1
+                                </span>
+                                <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
+                                    +
+                                </button>
+                            </div>
+                        </div>
+                        <button className=' bg-black text-white rounded-full p-2'>
+                            Cart
                         </button>
                     </div>
-                    </div>
-                    <button className=' bg-black text-white rounded-full p-2'>
-                        Cart
-                    </button>
-                </div>
+                )}
 
                 <h3 className='mt-2 text-lg font-semibold'>{product.name}</h3>
             </div>
