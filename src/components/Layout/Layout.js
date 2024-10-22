7cc0b97562048e6d8a677f568c3b46ea14a7c11d
vladadime <dime.97@hotmail.com>
2024-10-22 20:04:20 +0200

    Change class to Layout container div

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
     return (
-        <div className='bg-white bg-[repeating-linear-gradient(to_right,_rgba(255,0,0,0.2),_rgba(255,0,0,0.2)_1px,_white_1px,_white_55px)] h-screen'>
+        <div className='bg-white bg-[repeating-linear-gradient(to_right,_rgba(255,0,0,0.2),_rgba(255,0,0,0.2)_1px,_white_1px,_white_55px)] h-full'>
             {children}
         </div>
     )
