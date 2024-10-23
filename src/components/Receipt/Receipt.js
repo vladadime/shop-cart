20382c9b7487e2f36a159a74df005655a2e2834b
vladadime <dime.97@hotmail.com>
2024-10-23 16:48:22 +0200

    Add Receipt component

--- src/components/Receipt/Receipt.js
+++ src/components/Receipt/Receipt.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React from 'react'
+import Button from '../Button/Button'
 
 const Receipt = () => {
-  return <div>Receipt</div>;
-};
+    return (
+        <div className='w-1/3 ms-16 bg-gray-300 bg-opacity-50 px-10 h-96'>
+            <p className='text-lg font-bold text-black mt-6'>
+                Tvoja narudžbina
+            </p>
+            <div className='flex justify-between mt-8 mb-4'>
+                <span className='text-black'>Ukupno</span>
+                <div className='flex flex-column'>
+                    <span className='me-1 font-medium text-black'>19.940</span>
+                    <span className='text-xs mb-auto text-black'>RSD</span>
+                </div>
+            </div>
+            <div className='flex justify-between mb-4'>
+                <span className='text-black'>Ušteda</span>
+                <div className='flex flex-column'>
+                    <span className='me-1 font-medium text-black'>-1.200</span>
+                    <span className='text-xs mb-auto text-black'>RSD</span>
+                </div>
+            </div>
+            <div className='flex justify-between mb-4'>
+                <span className='text-black'>Isporuka Daily Express*</span>
+                <div className='flex flex-column'>
+                    <span className='text-xs text-black'>Besplatna</span>
+                </div>
+            </div>
+            <div className='border border-b-gray-400 mb-4'></div>
+            <div className='flex justify-between mb-2'>
+                <span className='text-black'>Ukupno za uplatu</span>
+                <div className='flex flex-column'>
+                    <span className='me-1 font-medium text-black'>19.940</span>
+                    <span className='text-xs mb-auto text-black'>RSD</span>
+                </div>
+            </div>
+            <p className='text-xs text-black mb-8'>
+                Cena je sa uključenim PDV-om
+            </p>
+            <Button content='Prijavi se za brže plaćanje' />
+        </div>
+    )
+}
 
-export default Receipt;
+export default Receipt
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,31 +1,37 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { CartProduct } from '../../components'
-import { Link } from 'react-router-dom';
+import { CartProduct, Receipt } from '../../components'
+import { Link } from 'react-router-dom'
 const Cart = () => {
     const dispatch = useDispatch()
-    const cartItems = useSelector((state) => state.cart.items);
+    const cartItems = useSelector((state) => state.cart.items)
 
     console.log(cartItems)
     return (
-        <div className='container mx-auto p-4'>
+        <div className='container mx-auto p-4 h-screen'>
             <div className='flex justify-between mb-6'>
                 <div className='text-left'>
                     <span className='text-base font-bold'>Tvoja korpa</span>
                 </div>
 
-                <div className='text-right'>
+                <div className='absolute top-5 right-5'>
                     <Link to='/products'>
                         <button>Products</button>
                     </Link>
                 </div>
             </div>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                {cartItems && cartItems.length
-                    ? cartItems.map((product) => (
-                          <CartProduct key={product.productID} product={product} />
-                      ))
-                    : null}
+            <div className='md:flex'>
+                <div className='flex flex-col w-3/5'>
+                    {cartItems && cartItems.length
+                        ? cartItems.map((product) => (
+                              <CartProduct
+                                  key={product.productID}
+                                  product={product}
+                              />
+                          ))
+                        : null}
+                </div>
+                <Receipt />
             </div>
         </div>
     )
