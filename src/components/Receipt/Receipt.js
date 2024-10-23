fb6df90ac5bbec565081e55b09d298a52f350849
vladadime <dime.97@hotmail.com>
2024-10-23 20:40:34 +0200

    Add logic for total count of cart

--- src/components/Receipt/Receipt.js
+++ src/components/Receipt/Receipt.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Button from '../Button/Button'
+import Modal from '../Modal/Modal'
+import { useState } from 'react'
+import { clearCart } from '../../store/cart/cartActions'
+import { useNavigate } from 'react-router-dom'
+const Receipt = ({ countPrice }) => {
+    const cartItems = useSelector((state) => state.cart.items)
+    const [isModalOpen, setModalOpen] = useState(false)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-const Receipt = () => {
+    const handlePurchase = () => {
+        if (cartItems.length) {
+            setModalOpen(true)
+        }
+    }
+
+    const closeModal = () => {
+        dispatch(clearCart())
+        setModalOpen(false)
+        navigate('/')
+    }
+
+    const totalCount = () => {
+        const sum = cartItems.reduce((acc, item) => {
+            return acc + countPrice(item)
+        }, 0)
+
+        return sum
+    }
+
+    const totalSaving = () => {
+        const sum = cartItems.reduce((acc, item) => {
+            return acc + item.productPrice * item.quantity
+        }, 0)
+
+        return Number(parseFloat(totalCount() - sum).toFixed(2))
+    }
+
     return (
         <div className='w-1/3 ms-16 bg-gray-300 bg-opacity-50 px-10 h-96'>
             <p className='text-lg font-bold text-black mt-6'>
@@ -10,14 +46,18 @@ const Receipt = () => {
             <div className='flex justify-between mt-8 mb-4'>
                 <span className='text-black'>Ukupno</span>
                 <div className='flex flex-column'>
-                    <span className='me-1 font-medium text-black'>19.940</span>
+                    <span className='me-1 font-medium text-black'>
+                        {totalCount()}
+                    </span>
                     <span className='text-xs mb-auto text-black'>RSD</span>
                 </div>
             </div>
             <div className='flex justify-between mb-4'>
                 <span className='text-black'>Ušteda</span>
                 <div className='flex flex-column'>
-                    <span className='me-1 font-medium text-black'>-1.200</span>
+                    <span className='me-1 font-medium text-black'>
+                        {totalSaving()}
+                    </span>
                     <span className='text-xs mb-auto text-black'>RSD</span>
                 </div>
             </div>
@@ -31,14 +71,20 @@ const Receipt = () => {
             <div className='flex justify-between mb-2'>
                 <span className='text-black'>Ukupno za uplatu</span>
                 <div className='flex flex-column'>
-                    <span className='me-1 font-medium text-black'>19.940</span>
+                    <span className='me-1 font-medium text-black'>
+                        {totalCount()}
+                    </span>
                     <span className='text-xs mb-auto text-black'>RSD</span>
                 </div>
             </div>
             <p className='text-xs text-black mb-8'>
                 Cena je sa uključenim PDV-om
             </p>
-            <Button content='Prijavi se za brže plaćanje' />
+            <Button
+                content='Prijavi se za brže plaćanje'
+                onClick={handlePurchase}
+            />
+            <Modal isOpen={isModalOpen} onClose={closeModal} />
         </div>
     )
 }
