06a7821b39d40bea423c87381f4a616ac717763d
vladadime <dime.97@hotmail.com>
2024-10-23 23:56:16 +0200

    Cart component - display message when cart is empty

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -22,15 +22,25 @@ const Cart = () => {
 
             <div className='lg:flex'>
                 <div className='flex flex-col w-full lg:w-3/5'>
-                    {cartItems && cartItems.length
-                        ? cartItems.map((product) => (
-                              <CartProduct
-                                  key={product.productID}
-                                  product={product}
-                                  countPrice={countPrice}
-                              />
-                          ))
-                        : null}
+                    {cartItems && cartItems.length ? (
+                        cartItems.map((product) => (
+                            <CartProduct
+                                key={product.productID}
+                                product={product}
+                                countPrice={countPrice}
+                            />
+                        ))
+                    ) : (
+                        <div className='bg-gray-300 bg-opacity-50 h-96 px-2 sm:px-10 lg:px-6 xl:px-10 mt-6 flex flex-col justify-center'>
+                            <p>
+                                Vaša korpa je trenutno prazna. Dodajte proizvode
+                                da biste nastavili sa kupovinom.
+                                <Link className='text-blue-500' to='/'>
+                                    Idi na Proizvode
+                                </Link>
+                            </p>
+                        </div>
+                    )}
                 </div>
                 <div className='w-full lg:w-1/3 lg:ms-16 '>
                     <Receipt countPrice={countPrice} />
