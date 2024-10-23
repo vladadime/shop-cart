a119aa763f37d263e81da974de67aa538aadca66
vladadime <dime.97@hotmail.com>
2024-10-23 09:22:59 +0200

    Change font size for Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ content }) => {
     return (
         <button
-            className='bg-black text-white text-xs rounded-full px-10 py-2 w-full'
+            className='bg-black text-white text-base rounded-full px-10 py-2 w-full'
             type='submit'
         >
             {content}
