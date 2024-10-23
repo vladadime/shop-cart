b77a1213f3f3f800d635a1d828664e3f812eef02
vladadime <dime.97@hotmail.com>
2024-10-23 09:25:21 +0200

    Add product quantity to data

--- src/data/data.js
+++ src/data/data.js
@@ -15,96 +15,112 @@ export const products = [
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product1,
+        productQuantity: "750g"
     },
     {
         productID: 2,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product2,
+        productQuantity: "750g"
     },
     {
         productID: 3,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product3,
+        productQuantity: "750g"
     },
     {
         productID: 4,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product4,
+        productQuantity: "750g"
     },
     {
         productID: 5,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product5,
+        productQuantity: "750g"
     },
     {
         productID: 6,
         productLabel: 'Kikiriki puter',
         productPrice: 546,
         productImage: product4,
+        productQuantity: "750g"
     },
     {
         productID: 7,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product6,
+        productQuantity: "750g"
     },
     {
         productID: 8,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product5,
+        productQuantity: "750g"
     },
     {
         productID: 9,
         productLabel: 'Kikiriki puter',
         productPrice: 546,
         productImage: product7,
+        productQuantity: "750g"
     },
     {
         productID: 10,
         productLabel: 'Kikiriki puter',
         productPrice: 546,
         productImage: product8,
+        productQuantity: "750g"
     },
     {
         productID: 11,
         productLabel: 'Kikiriki puter',
         productPrice: 546,
         productImage: product9,
+        productQuantity: "750g"
     },
     {
         productID: 12,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product7,
+        productQuantity: "750g"
     },
     {
         productID: 13,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product5,
+        productQuantity: "750g"
     },
     {
         productID: 14,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product10,
+        productQuantity: "750g"
     },
     {
         productID: 15,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product6,
+        productQuantity: "750g"
     },
     {
         productID: 16,
         productLabel: 'Naturela sa Rogačem i Agava Šećerom',
         productPrice: 546,
         productImage: product5,
+        productQuantity: "750g"
     },
 ]
 
