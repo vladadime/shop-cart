ed4e0aede85246b8fc4255c854a88f4fea6b865f
vladadime <dime.97@hotmail.com>
2024-10-23 20:39:36 +0200

    Add logic for change quantity for CartProduct component

--- src/components/CartProduct/CartProduct.js
+++ src/components/CartProduct/CartProduct.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useDispatch } from 'react-redux'
+import {
+    removeProductFromCart,
+    updateProductQuantity,
+} from '../../store/cart/cartActions.js'
+const CartProduct = ({ product, countPrice }) => {
+    const dispatch = useDispatch()
 
-const CartProduct = ({ product }) => {
+    const increaseProductCounter = () => {
+        dispatch(updateProductQuantity(product.productID, product.quantity + 1))
+    }
+
+    const decreaseProductCounter = () => {
+        dispatch(
+            updateProductQuantity(
+                product.productID,
+                Math.max(product.quantity - 1, 1),
+            ),
+        )
+    }
+
+    const handleRemoveFromCart = () => {
+        dispatch(removeProductFromCart(product.productID))
+    }
+
     return (
         <div className='border-gray-400 border-b mt-0'>
             <div className='h-40 flex my-8'>
@@ -14,24 +36,38 @@ const CartProduct = ({ product }) => {
                 <div className='w-3/5 px-8'>
                     <div className='flex flex-col space-y-8'>
                         <div className='mb-10 flex flex-col'>
-                            <span className='font-bold'>{product.productLabel}</span>
-                            <span className=''>{product.productQuantity}</span>
+                            <span className='font-bold'>
+                                {product.productLabel}
+                            </span>
+                            <span>{product.productQuantity}</span>
+                            <span className='text-xs text-gray-500'>
+                                {product.productPrice} RSD po kom.
+                            </span>
                         </div>
                         <div className='flex'>
                             <div className='rounded-full border border-black bg-white me-2 px-4 w-28'>
                                 <div className='flex justify-around'>
-                                    <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
+                                    <button
+                                        className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'
+                                        onClick={decreaseProductCounter}
+                                    >
                                         -
                                     </button>
                                     <span className='mx-2 text-base my-auto'>
-                                        1
+                                        {product.quantity}
                                     </span>
-                                    <button className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'>
+                                    <button
+                                        className='text-gray-600 p-1 rounded-full hover:bg-gray-200 text-2xl'
+                                        onClick={increaseProductCounter}
+                                    >
                                         +
                                     </button>
                                 </div>
                             </div>
-                            <button className='p-1 text-base underline'>
+                            <button
+                                className='p-1 text-base underline'
+                                onClick={handleRemoveFromCart}
+                            >
                                 Ukloni
                             </button>
                         </div>
@@ -40,10 +76,20 @@ const CartProduct = ({ product }) => {
                 <div className='w-1/5'>
                     <div className='flex flex-column'>
                         <span className='me-1 font-medium text-2xl'>
-                            {product.productPrice}
+                            {countPrice(product)}
                         </span>
                         <span className='text-xs mb-auto'>RSD</span>
                     </div>
+                    {product.discount ? (
+                        <div className='flex flex-column'>
+                            <span className='me-1 text-red-600 font-medium text-base line-through'>
+                                {product.productPrice * product.quantity}
+                            </span>
+                            <span className='text-xs text-red-600 mb-auto'>
+                                RSD
+                            </span>
+                        </div>
+                    ) : null}
                 </div>
             </div>
         </div>
