cb0f09a3e60afbd8d287f6962c6728c38fc7fae3
vladadime <dime.97@hotmail.com>
2024-10-23 23:57:07 +0200

    fixing responsive design and displaying Header component if user logged in

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import { Header } from '../Header'
-
+import { useSelector } from 'react-redux'
 const Layout = ({ children }) => {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
     return (
-        <div className='bg-white bg-[repeating-linear-gradient(to_right,_rgba(255,0,0,0.2),_rgba(255,0,0,0.2)_1px,_white_1px,_white_55px)]'>
-            <Header />
-            <div className='container mx-auto p-10'>{children}</div>
+        <div className='h-full bg-white bg-[repeating-linear-gradient(to_right,_rgba(255,0,0,0.2),_rgba(255,0,0,0.2)_1px,_white_1px,_white_55px)]'>
+            {isLoggedIn && <Header />}
+            <div className='container mx-auto p-10 min-h-screen'>{children}</div>
         </div>
     )
 }
